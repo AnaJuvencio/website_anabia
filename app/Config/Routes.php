caf9abd69e0a4d2715049b8004fc588b869a746b
AnaJuvencio <ana.juvencio01@gmail.com>
2023-11-19 22:16:12 -0300

    tentando configurar a URL em htaccess

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setDefaultController('Home');
 $routes->get('/', 'Home::index');
+$routes->set404Override('App\Controllers\Your404Controller::index');
+
---
+++ app/Views/pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+</head>
+
+<body>
+  <?php include 'components/header.php'; ?>
+ 
+    <!-- Template Main JS File -->
+</body>
